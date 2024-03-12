c5034a051a00179724241dbd6f5a70c81e058b65
wcollins <william.levi.collins@gmail.com>
2024-03-12 15:50:53 -0400

    Add variables.tf

---
+++ 06-onboarding-ipsec-connectors/variables.tf
@@ -0,0 +1,13 @@
+variable "alk_portal" {
+  type        = string
+}
+
+variable "alk_username" {
+  type        = string
+  sensitive   = true
+}
+
+variable "alk_password" {
+  type        = string
+  sensitive   = true
+}
