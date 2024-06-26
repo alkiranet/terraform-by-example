cf87569a7f28ae4f0048a71fc76f9b94498c3adf
wcollins <william.levi.collins@gmail.com>
2024-06-26 11:24:12 -0400

    Update versions.tf

--- 05-onboard-with-gitlab-cicd/versions.tf
+++ 05-onboard-with-gitlab-cicd/versions.tf
@@ -1,10 +1,11 @@
 terraform {
+  required_version = ">= 1.8.5"
 
   required_providers {
 
     alkira = {
       source  = "alkiranet/alkira"
-      version = "1.2.2"
+      version = "1.2.5"
     }
 
   }
