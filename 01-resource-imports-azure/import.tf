b536df346fccd52cb894a16cbb21c3f3c29717ed
wcollins <william.levi.collins@gmail.com>
2024-06-27 07:56:53 -0400

    Add import.tf

---
+++ 01-resource-imports-azure/import.tf
@@ -0,0 +1,17 @@
+# import azure vnet
+import {
+  to = azurerm_virtual_network.vnet_a
+  id = ""
+}
+
+# import azure subnet
+import {
+  to = azurerm_subnet.subnet_a
+  id = ""
+}
+
+# import alkira connector
+import {
+  to = alkira_connector_azure_vnet.vnet_a
+  id = ""
+}
