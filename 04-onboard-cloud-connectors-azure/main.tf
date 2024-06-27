0de00d83fa3c7bcf67397305f5f6e445bafdc4de
wcollins <william.levi.collins@gmail.com>
2024-06-27 07:59:16 -0400

    Update main.tf

--- 04-onboard-cloud-connectors-azure/main.tf
+++ 04-onboard-cloud-connectors-azure/main.tf
@@ -49,25 +49,3 @@ resource "alkira_connector_azure_vnet" "training" {
   ]
 
 }
-
-# resource "alkira_connector_aws_vpc" "training" {
-#   name           = format("connector-%s", local.aws_vpc_name)
-#   vpc_id         = aws_vpc.training.id
-#   vpc_cidr       = [aws_vpc.training.cidr_block]
-
-#   aws_account_id = var.aws_account_id
-#   aws_region     = var.aws_region
-
-#   credential_id  = alkira_credential_aws_vpc.training.id
-#   cxp            = "US-EAST-2"
-#   group          = data.alkira_group.training.name
-#   segment_id     = data.alkira_segment.training.id
-#   size           = "SMALL"
-
-#   vpc_route_table {
-#     id       = aws_vpc.training.default_route_table_id
-#     options  = "ADVERTISE_DEFAULT_ROUTE"
-#   }
-
-
-# }
